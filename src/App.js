5bcc8f138ab7ee8c052ed7a4833f8797a686c047
kmcpeake <kmcpeake@juniper.net>
2020-06-26 18:08:01 +0100

    Updated VS Code format settings like
    18:08 $ more ~/Library/Application\ Support/Code/User/settings.json
    {
      "files.associations": {
        "*.rmd": "markdown"
      },
      "editor.renderControlCharacters": true,
      "editor.codeActionsOnSave": {
        "source.fixAll": true
      },
    
      "[javascript]": {
        "editor.formatOnSave": false
      },
    
      "eslint.alwaysShowStatus": true,
      "eslint.codeActionsOnSave.mode": "all",
      "prettier.disableLanguages": ["js"],
      "files.autoSave": "onFocusChange",
      "editor.formatOnSave": true,
      "[jsonc]": {
        "editor.defaultFormatter": "esbenp.prettier-vscode"
      }
    }

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Shining Eibhlin
-        </p>
+        <p>Shining Eibhlin</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello Aileen :-) 
+          Hello Aileen :-)
         </a>
       </header>
     </div>
