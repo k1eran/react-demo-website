122a62e30ddce1e8d752e0583da0e97a6b2378bc
kmcpeake <kmcpeake@juniper.net>
2020-06-29 13:33:57 +0100

    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions

--- src/index.js
+++ src/index.js
@@ -11,12 +11,7 @@ class Square extends React.Component {
   }
   render() {
     return (
-      <button
-        className="square"
-        onClick={function () {
-          alert("click");
-        }}
-      >
+      <button className="square" onClick={() => alert("click")}>
         {this.props.value}
       </button>
     );
