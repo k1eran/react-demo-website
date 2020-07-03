a89256b762ed8a5187caeb36111ff242a76c0c93
kmcpeake <kmcpeake@juniper.net>
2020-07-03 13:56:07 +0100

    Added https://reactjs.org/tutorial/tutorial.html#implementing-time-travel

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ function Square(props) {
       className="square"
       onClick={() => {
         props.onClick();
-        console.log("X hit");
+        console.log("... you clicked square with props=", props);
       }}
     >
       {props.value}
@@ -97,13 +97,24 @@ class Game extends React.Component {
         },
       ],
       xIsNext: true,
+      stepNumber: 0,
     };
   }
 
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
+
+    const moves = history.map((currentVal, index) => {
+      const desc = index ? "Go to move " + index : "Go to start";
+      return (
+        <li key={index}>
+          <button onClick={() => this.jumpTo(index)}>{desc}</button>
+        </li>
+      );
+    });
+
     let status;
     if (winner) {
       status = "Winner: " + winner;
@@ -121,14 +132,14 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
   }
 
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const sqsCopy = current.squares.slice();
 
@@ -151,11 +162,18 @@ class Game extends React.Component {
           squares: sqsCopy,
         },
       ]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
     });
   }
-}
 
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: step % 2 === 0,
+    });
+  }
+}
 // ========================================
 
 ReactDOM.render(
